a9b6a90bf1f349f471195d6078b2badbb9e7c990
juniar <juniar.rahman@rithm.nl>
2020-08-23 00:23:18 +0700

    added more stuff

--- reset_eval.sh
+++ reset_eval.sh
@@ -5,7 +5,7 @@ OS_NAME=$(uname -s)
 JB_PRODUCTS="IntelliJIdea CLion PhpStorm GoLand PyCharm WebStorm Rider DataGrip RubyMine AppCode"
 
 for PRD in $JB_PRODUCTS; do
-	rm -rf ~/.java/.userPrefs
+	rm -rf ~/.java/.userPrefs/jetbrains
 	rm -rf ~/.config/JetBrains/${PRD}*/eval/
 	rm -rf ~/.config/JetBrains/${PRD}*/options/other.xml
 done
