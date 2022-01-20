d0de0d5f8c1f25c5fe17f26af0486b66aea056f3
juniar <juniar.rahman@rithm.nl>
2022-01-20 16:50:06 +0700

    added more stuff

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ln -s ~/dot_files/.alacritty.yml ~/.alacritty.yml
+ln -s ~/dot_files/.bash_aliases ~/.bash_aliases
+ln -s ~/dot_files/.bash_logout ~/.bash_logout
+ln -s ~/dot_files/.bash_profile ~/.bash_profile
+ln -s ~/dot_files/.bashrc ~/.bashrc
+ln -s ~/dot_files/.ideavimrc ~/.ideavimrc
+ln -s ~/dot_files/.tmux.conf ~/.tmux.conf
+ln -s ~/dot_files/.vimrc ~/.vimrc
+ln -s ~/dot_files/.vimrc.keymap ~/.vimrc.keymap
+ln -s ~/dot_files/.zshrc ~/.zshrc
